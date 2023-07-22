9138571dce86a51e4e5b684d8c8b0082f9172221
Andres <softwareapgm@gmail.com>
2023-07-22 14:23:43 -0400

    Started missingOptions method

--- src/configParser.sh
+++ src/configParser.sh
@@ -2,6 +2,11 @@
 
 file="/home/jairo/Development/2_Runssh/config/config.runssh"
 
+
+<<comment
+Paremeters:(string filename)
+Returns: [string] prfx
+comment
 function getPrefixes(){
 	local -a prfx=()
 	#Search prefix then get 2nd field after > and replace ":" for whitespace
@@ -9,6 +14,10 @@ function getPrefixes(){
 	echo ${prfx[@]}
 }
 
+<<comment
+Paremeters: (string filename, string Prefix)
+Returns: [string] opts
+comment
 function getOptions(){
 	local -a opts=()
 	fileName=$1
@@ -16,25 +25,65 @@ function getOptions(){
 	echo ${opts[@]}
 }
 
+<<comment
+Paremeters:(string Option)
+Returns: string attribute
+comment
 function getAttributes(){
-	echo $(echo $1 | cut -d ">" -f 1)
+	attribute=$(echo $1 | cut -d ">" -f 1)
+	echo $attribute
 }
 
+<<comment
+Paremeters:(string Option)
+Returns: string attribute
+comment
 function getValues(){
-	echo $(echo $1 | cut -d ">" -f 2)
+	value=$(echo $1 | cut -d ">" -f 2)
+	echo $value
+}
+
+<<comment
+Paremeters:(string filename, [string] prefixes)
+Returns: bool
+comment
+function missingOptions(){
+	# file=$1
+	local -a prfx=()
+	# prfx=("$@")
+
+	#prfx=("${prfx[@]:1:2}" "${prfx[@]:3:1}")
+
+	prfx=("${@:2:$#}")
+	# "${@:3:1}")
+
+	echo ${prfx[@]}
+
+	for p in ${prfx[@]}
+	do
+		if grep -q ^$p $1
+		then
+			missing=false
+		else
+			missing=true
+		fi
+	done
+
 }
 
 items=( $(getPrefixes $file) )
 
-#echo ${#items[@]}
 
 #getOptions $file "1_"
 
-for i in ${items[@]}
-do
-	for opt in $(getOptions $file $i)
-	do
-		echo $(getAttributes $opt)
-		echo $(getValues $opt)
-	done
-done
+# for i in ${items[@]}
+# do
+# 	for opt in $(getOptions $file $i)
+# 	do
+# 		echo $(getAttributes $opt)
+# 		echo $(getValues $opt)
+# 	done
+# done
+
+
+missingOptions $file ${items[@]}
