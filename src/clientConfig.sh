02f336556834020c4aa2f767a23c8eb0bc31f8bc
Andres <softwareapgm@gmail.com>
2023-08-16 19:17:34 -0400

    Add source resolver

--- src/clientConfig.sh
+++ src/clientConfig.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 
-source ./configParser.sh
+# ----------- DECLARE DEPENDENCIES -----------
+
+Dependencies=("configParser.sh")
+
+# ----------- SOLVE FILE DEPENDENCY -----------
+
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+BASE_DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+# -----------
+
+for file in ${Dependencies[@]}
+do
+    file=$BASE_DIR"/"$file
+    if [ ! -f $file ]
+    then
+
+        msg_error "The below file cannot be found: \n $file"
+        exit 1
+    fi
+    source $file
+done
+
+# ----------- START SCRIPT -----------
+
 
 ##
 # Parameters: (string filename)
